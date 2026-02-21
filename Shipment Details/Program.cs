e60aa04c8516084ad4ebcb9073ccb8d2f7894d38
Cesar789 <philiphermosa087@gmail.com>
2026-02-21 11:42:58 +0800

    Initial commit

---
+++ Shipment Details/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Shipment_Details
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Order confirmation");
+            Console.Write("Enter your Name: ");
+            string buyer = Console.ReadLine();
+            Console.Write("Enter your Contact +63 ");
+            int number = (int)Convert.ToInt64(Console.ReadLine());
+            Console.Write("Enter your Address: ");
+            string address = Console.ReadLine();
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("E-commerce store");
+            Console.WriteLine("Product for example : Apple");
+            Console.WriteLine("Price:   " + "quantity: ");
+            Console.WriteLine("");
+            Console.WriteLine("Range of date to receive");
+            Console.WriteLine("");
+            Console.WriteLine("Shop discount: ");
+            Console.WriteLine("");
+            Console.WriteLine("Order Summary");
+            Console.WriteLine("Product Subtotal: ");
+            Console.WriteLine("Shipping Subtotal: ");
+            Console.WriteLine("Total: ");
+            Console.WriteLine("");
+            Console.WriteLine("Payment Method");
+            Console.WriteLine("                         Place Order");
+        }
+    }
+}
